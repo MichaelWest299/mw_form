c11c8f0dbc69e13b9767bddcd12871e7d835bfb4
Michael West <michaelwest299@gmail.com>
2016-10-08 19:39:02 +0100

    Prevent enter key form submission

--- src/js/submitHandler.js
+++ src/js/submitHandler.js
@@ -2,8 +2,12 @@
 //Uses serializeObject.js to obtain form data and transform it into a JSON object
 (function() {
   $(function() {
+    //Prevent enter key form submission
+    $('form input').on('keypress', function(e) {
+      return e.which !== 13;
+    });
+    //Create json object upon form submission
     $('form').submit(function() {
-      //Create json object upon form submission
       var answerData = $(this).serializeObject();
       console.log(answerData);
       // $('#output').text(JSON.stringify(answerData, undefined, 2));
