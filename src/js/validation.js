4e243a0c011e17cf5e1c80fb4e685cfe9a7bf6c5
Michael West <michaelwest299@gmail.com>
2016-10-13 15:33:23 +0100

    Implementing module pattern in validation.js

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,65 +1,81 @@
 (function(){
 
-  //Set of regex objects
-  var regexpressions = {
-  	name: {
-  		regex: /^[a-zA-Z ]{2,30}$/,
-  		elementid: ".details label input[type=text]",
-  	},
-  	email: {
-  		regex: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-  		elementid: ".details label input[type=email]"
-  	},
-  	telephone: {
-      regex: /^(?:[-(). +]*[0-9]){11,}[-\s\.()+]{0,}[-\s\./0-9]*$/,
-      elementid: ".details label input[type=tel]"
-  	},
-    url: {
-      regex: /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/,
-      elementid: ".details label input[type=url]"
+  var validation = {
+    regexpressions : {
+      text : /^[a-zA-Z ]{2,30}$/,
+      email:  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      tel:  /^(?:[-(). +]*[0-9]){11,}[-\s\.()+]{0,}[-\s\./0-9]*$/,
+      url: /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/,
+    },
+    
+    init: function() {
+        this.cacheDom();
+        this.validate();
+    },
+
+    cacheDom: function() {
+      this.details = $('.details');
+      this.detailsNext = $('.next-details');
+      this.inputElement = this.details.find('input');
+      this.hasSuccess = this.details.find('.form-control-success');
+
+    },
+
+    validate: function() {
+      for(var i = 0; i < this.inputElement.length; i++)
+      {
+        this.applyRegExByType(this.inputElement[i]);
+      }
+    },
+
+    applyRegExByType: function(inputElement)
+    {
+      for(var regExType in validation.regexpressions) {
+        var regex = validation.regexpressions[regExType];
+        if(inputElement.type == regExType) {
+            this.setInputFieldRegexType(regex, inputElement);
+        }
+      }
+    },
+
+    setInputFieldRegexType: function(regex, inputElement)
+    {
+      var self = this;
+      $(inputElement).on('input', function(e) {
+        var test = regex.test($(inputElement).val());
+        self.setValidationClasses(inputElement, test);
+      });
+    },
+
+    setValidationClasses: function(inputElement, test) {
+      this.removeExistingClasses(inputElement);
+      if(test) {
+          $(inputElement).parents(':eq(1)').addClass('has-success');
+          $(inputElement).addClass('form-control-success');
+        } else {
+          $(inputElement).parents(':eq(1)').addClass('has-danger');
+          $(inputElement).addClass('form-control-danger');
+      }
+      this.validateNextButton();
+    },
+
+    removeExistingClasses: function(inputElement) {
+      $(inputElement).removeClass('form-control-danger form-control-success');
+      $(inputElement).parents(':eq(1)').removeClass('has-success has-danger');
+    },
+
+    validateNextButton: function() {
+      this.cacheDom();
+      if (this.hasSuccess.length == this.details.length) {
+        this.detailsNext.prop("disabled", null);
+        this.detailsNext.css("border-color","green");
+      } else {
+        this.detailsNext.prop("disabled", true);
+        this.detailsNext.css("border-color","red");
+      }
     }
   };
 
-  //Our main validation function for each detail
-  var testThis = function(regEx, elementId)
-  {
-    //Returns true if validation passes
-    function validateInput(inputValue) {
-      return regEx.test(inputValue);
-    }
-    //Add tick or cross depending whether input is correct
-    function validate() {
-      var inputValue = $(elementId).val();
-      if (validateInput(inputValue)) {
-        $(elementId).closest('.details').addClass('has-success');
-        $(elementId).addClass('form-control-success');
-      } else {
-        $(elementId).closest('.details').addClass('has-danger');
-        $(elementId).addClass('form-control-danger');
-      }
-    }
-    //Remove any pre-existing validation classes before validating
-    $(elementId).keyup(function(){
-        $(elementId).removeClass('form-control-danger form-control-success');
-        $(elementId).closest('.details').removeClass('has-success has-danger');
-        validate();
-    });
-  };
+validation.init();
 
-  //Feed in each regex object into the above function so that all details are validated
-  for (var key in regexpressions) {
-  		var obj = regexpressions[key];
-  		testThis(obj.regex, obj.elementid);
-  }
-
-  //Stops the user from proceeding without all valid detail fields
-  $(".details label input").keyup(function(){
-      if ($(".details.has-success").length == $(".details").length) {
-        $('.next-details').prop("disabled", null);
-        $('.next-details').css("border-color","green");
-      } else {
-        $('.next-details').prop("disabled", true);
-        $('.next-details').css("border-color","red");
-      }
-  });
 })();
